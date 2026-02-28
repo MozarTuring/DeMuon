75e38fab2adf1e39629f12360a5ff12eb4ad4eb7
maojingwei@macbook <jingwei_m@163.com>
2026-02-28 14:13:26 +0100

    v

--- remote.sh
+++ remote.sh
@@ -4,10 +4,9 @@ if false; then
     #local
     cd /Users/maojingwei/baidu/project/ && source common_tools/meta_script.sh alvis1 DeMuon slurm
 
-rsync -av alvis1:~/project_remote_jwm/fed193bc30219e80a93aec1286e88be4693d184c/ /Users/maojingwei/baidu/project/zzzjwmoutput/Decentralized-Training-Exp/runs/fed193bc30219e80a93aec1286e88be4693d184c
     # remote
     exit
     ssh alvis1
-    scancel 6007399
+    scancel 6011615
     squeue --me
 fi
